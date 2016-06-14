095de794701a1ee6d7ab66aa4bc76f429879dbbc
Max Strübing <max_struebing@gmx.de>
2016-06-14 12:54:23 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env bash
+
+find . -type f -not -iwholename '*git*' -not -iwholename './install.sh' > filelist
+sed -i 's|^\.||' filelist
+sed -i 's|/install.sh||' filelist
+
+echo /.gitconfig >> filelist
+
+while read file          
+do
+  ln -sf $(pwd)$file $HOME$file
+done < filelist
+
+rm filelist
