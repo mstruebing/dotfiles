12b7c62b479bf4fb43e13ff393b88dd77fd50328
Max Strübing <mxstrbng@gmail.com>
2024-03-07 17:04:34 +0100

    skhd and yabai

--- nvim_lsp/.config/nvim/after/plugin/lsp.lua
+++ nvim_lsp/.config/nvim/after/plugin/lsp.lua
@@ -4,6 +4,7 @@ lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
