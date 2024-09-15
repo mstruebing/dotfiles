b7c540cddc71142c73177a057c5b3383629c7cb4
Max Strübing <mxstrbng@gmail.com>
2024-09-15 12:30:54 +0200

    change ts server

--- nvim_lsp/.config/nvim/after/plugin/lsp.lua
+++ nvim_lsp/.config/nvim/after/plugin/lsp.lua
@@ -28,7 +28,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer'},
+  ensure_installed = {'ts_ls', 'rust_analyzer'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
